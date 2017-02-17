8e9d0754526634ae7d130d2c8c4d2535de142508
fRandOmizer <zan.richard@gmail.com>
2017-02-17 18:06:22 +0100

    Update - vyber bodu + vykreslenie oblasti

---
+++ GUI/build/classes-generated/cz/fidentis/gui/actions/Bundle.java
@@ -0,0 +1,76 @@
+package cz.fidentis.gui.actions;
+/** Localizable strings for {@link cz.fidentis.gui.actions}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>New Ageing</i>
+     * @see AgeingActionListener
+     */
+    static String CTL_AgeingActionListener() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_AgeingActionListener");
+    }
+    /**
+     * @return <i>Choose background...</i>
+     * @see Background
+     */
+    static String CTL_Background() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Background");
+    }
+    /**
+     * @return <i>Exit</i>
+     * @see Exit
+     */
+    static String CTL_Exit() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Exit");
+    }
+    /**
+     * @return <i>Export...</i>
+     * @see Export
+     */
+    static String CTL_Export() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Export");
+    }
+    /**
+     * @return <i>Open Project...</i>
+     * @see OpenProject
+     */
+    static String CTL_OpenProject() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenProject");
+    }
+    /**
+     * @return <i>Reset application settings</i>
+     * @see ResetApplicationSettings
+     */
+    static String CTL_ResetApplicationSettings() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ResetApplicationSettings");
+    }
+    /**
+     * @return <i>Save All</i>
+     * @see SaveAll
+     */
+    static String CTL_SaveAll() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_SaveAll");
+    }
+    /**
+     * @return <i>Save Project</i>
+     * @see SaveProject
+     */
+    static String CTL_SaveProject() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_SaveProject");
+    }
+    /**
+     * @return <i>Save Project As...</i>
+     * @see SaveProjectAs
+     */
+    static String CTL_SaveProjectAs() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_SaveProjectAs");
+    }
+    /**
+     * @return <i>Textures</i>
+     * @see Textures
+     */
+    static String CTL_Textures() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Textures");
+    }
+    private void Bundle() {}
+}
